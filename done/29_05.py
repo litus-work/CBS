8a4a9eb872f80eec6aa7cc10c5c83c962c08b57d
Serhii Litus <your@email.com>
2025-06-03 18:13:14 +0200

    03_06_HW

--- done/29_05.py
+++ done/29_05.py
@@ -1,13 +1,13 @@
 import random
 
-with open("numbers.txt", "w") as file:
+with open("../numbers.txt", "w") as file:
     for _ in range(10000):
         number = random.uniform(0, 1000)
         file.write(f"{number}\n")
 
 total = 0.0
 
-with open("numbers.txt", "r") as file:
+with open("../numbers.txt", "r") as file:
     for line in file:
         total += float(line.strip())
 
--- test.py
+++
@@ -1,5 +0,0 @@
-import re
-string = "Test1 Test2 Test3 Test4 Test5"
-result = re.findall(r"Test", string)
-print("Список усіх знайдених співпадінь:", result)
-print("Кількість усіх співпадінь =", len(result))
