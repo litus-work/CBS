b9aa09e8819d99e3a8dc4180027d3e367e68f4b1
Serhii Litus <your@email.com>
2025-08-05 13:24:18 +0200

    imagies for products was added

--- done/Django_Store/store/core/views.py
+++ done/Django_Store/store/core/views.py
@@ -5,19 +5,21 @@ from .models import Product, Category
 
 
 def product_list(request, category_slug=None):
+    categories = Category.objects.prefetch_related('products__images')  # оптимизация
     category = None
-    categories = Category.objects.all()
-    products = Product.objects.all()
 
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
-        products = products.filter(category=category)
+        products = category.products.all()
+    else:
+        products = Product.objects.all()
 
     return render(request, 'core/product_list.html', {
-        'category': category,
         'categories': categories,
-        'products': products
+        'category': category,
+        'products': products,
     })
+
 
 def product_detail(request, slug):
     product = get_object_or_404(Product, slug=slug)
