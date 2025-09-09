9fdef4467f1cee57f9791af0995a0a6772c2d72c
Serhii Litus <your@email.com>
2025-09-09 11:46:07 +0200

    Commit from Windows 09/09

--- done/Django_Store/store/store/urls.py
+++ done/Django_Store/store/store/urls.py
@@ -8,6 +8,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls', namespace='core')),
+    path('accounts/', include('accounts.urls', namespace='accounts')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
